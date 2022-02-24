699eba1adfb26d633e7d97ddb1f2eb43444485e3
joselara <jose@romantechnologies.co.uk>
2022-02-24 10:13:17 -0500

    added version service

---
+++ lib/services/version_service.dart
@@ -0,0 +1,35 @@
+import 'package:code_learn/globals/service.dart';
+import 'package:code_learn/launch.dart';
+import 'package:code_learn/services/router/router.dart';
+
+VersionService get versionService => locate<VersionService>();
+
+class VersionService {
+  double lastVersion = 0;
+
+  Future<void> updateVersion(double version) async {
+    try {
+      final result = await router.post(endpoint: '/version/$version');
+      if (result != null) {
+        if (result['version'] != null) {
+          lastVersion = result['version'];
+        }
+      }
+    } catch (e) {
+      logError(e);
+    }
+  }
+
+  Future<void> fetchLatestVersion() async {
+    try {
+      final result = await router.get(endpoint: '/version');
+      if (result != null) {
+        if (result['version'] != null) {
+          lastVersion = result['version'];
+        }
+      }
+    } catch (e) {
+      logError(e);
+    }
+  }
+}
